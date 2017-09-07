a85b1d2f166e0a8dd863ac4bccb2d0259b173a27
Erik <flyers_rock_87@hotmail.com>
2017-09-07 17:47:14 +0000

    error handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,62 +1,82 @@
 class User < ActiveRecord::Base
   def pick
-    driver = Selenium::WebDriver.for :phantomjs
-    
-    # Set wait function to allow browser time to react
-    wait = Selenium::WebDriver::Wait.new(:timeout => 60)
-
-    # Go to streak page
-    driver.navigate.to 'http://streak.espn.com/en/entry'
-    
-    # if current time is after or substantially before start times, end script without logging in
-    matchups = wait.until { driver.find_elements(:class, 'matchup-container') }
-    times = matchups.map { |m| m.find_element(:class, "startTime").attribute('data-locktime').to_datetime }
-    start = times[0]
-    finish = times[-1]
-    now = DateTime.now.in_time_zone("Eastern Time (US & Canada)")
-    if (now + 30.minutes) < start
-      puts "Too early; closing"
-      driver.close
-      return true
-    elsif now > finish
-      puts "No more games; closing"
-      driver.close
-      return true
-    else
-      puts "Checking games..."
-    end
-    
-    # LOG INTO ESPN
-    puts "Logging in..."
-    user_module = wait.until { driver.find_element(:class, 'user') }
-    login_button = wait.until { user_module.find_element(:tag_name, 'a') }
-    
-    wait.until { login_button.click }
-    
-    wait.until { driver.switch_to.frame('disneyid-iframe') }
-    
-    fields = wait.until { driver.find_elements(:tag_name, 'input') }
     retries = 0
     begin
-      fields.find { |f| f.attribute('placeholder')['Username'] }.send_keys self.username
-      fields.find { |f| f.attribute('placeholder')['Password'] }.send_keys self.password
+      driver = Selenium::WebDriver.for :phantomjs
+      
+      # Set wait function to allow browser time to react
+      wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+  
+      # Go to streak page
+      driver.navigate.to 'http://streak.espn.com/en/entry'
+      
+      # if current time is after or substantially before start times, end script without logging in
+      matchups = wait.until { driver.find_elements(:class, 'matchup-container') }
+      times = matchups.map { |m| m.find_element(:class, "startTime").attribute('data-locktime').to_datetime }
+      start = times[0]
+      finish = times[-1]
+      now = DateTime.now.in_time_zone("Eastern Time (US & Canada)")
+      if (now + 30.minutes) < start
+        puts "Too early; closing"
+        driver.close
+        return true
+      elsif now > finish
+        puts "No more games; closing"
+        driver.close
+        return true
+      else
+        puts "Checking games..."
+      end
+      
+      # LOG INTO ESPN
+      puts "Logging in..."
+      user_module = wait.until { driver.find_element(:class, 'user') }
+      login_button = wait.until { user_module.find_element(:tag_name, 'a') }
+      
+      wait.until { login_button.click }
+      
+      wait.until { driver.switch_to.frame('disneyid-iframe') }
+      
+      fields = wait.until { driver.find_elements(:tag_name, 'input') }
+      retries = 0
+      begin
+        fields.find { |f| f.attribute('placeholder')['Username'] }.send_keys u.username
+        fields.find { |f| f.attribute('placeholder')['Password'] }.send_keys u.password
+      rescue
+        if retries < 20
+          retries += 1
+          retry
+        else
+          puts "Failed to log in"
+          driver.close
+          return false
+        end
+      end
+      submit = wait.until { driver.find_elements(:tag_name, 'button').select { |b| b.text == "Log In" }[0] }
+      wait.until { submit.click }
+      puts "Successfully logged in!"
+      driver.switch_to.default_content
+      
+      # Wait until site is done fully rendering
+      sleep(10)
+      
+      # Run check to make sure login took
+      check = wait.until { driver.find_elements(:tag_name, 'a').map { |a| a.text } }
+      if check.include?("My Entry")
+        puts "Logged in; continuing"
+      else
+        raise
+      end
     rescue
-      if retries < 20
+      if retries < 5
+        puts "Login failed; trying again (#{retries})"
         retries += 1
         retry
       else
-        puts "Failed to log in"
-        driver.close
+        puts "Login failed 5 times; Closing."
         return false
       end
     end
-    submit = wait.until { driver.find_elements(:tag_name, 'button').select { |b| b.text == "Log In" }[0] }
-    wait.until { submit.click }
-    puts "Successfully logged in!"
-    driver.switch_to.default_content
-    
-    # Wait until site is done fully rendering
-    sleep(10)
     
     # Close if a pick is already active
     #### maybe use class pendingpick if it disappears when there is none
@@ -73,6 +93,7 @@ class User < ActiveRecord::Base
     rescue
       if retries < 10
         puts "Retrying (#{retries})"
+        retries += 1
         retry
       else
         puts "Failed to find elements 10 times"
@@ -87,7 +108,7 @@ class User < ActiveRecord::Base
     trends = matchups.map { |m| wait.until { m.find_elements(:class, 'wpw').map { |c| c.text.to_f }.uniq } }
     
     # Find the first matchup in which [THRESHOLD]% of people are selecting one winner
-    target = trends.find { |t| t[0] > self.threshold || t[1] > self.threshold }
+    target = trends.find { |t| t[0] > u.threshold || t[1] > u.threshold }
     
     # Find index of the predicted winner
     target[0] > target[1] ? target_selection_index = 0 : target_selection_index = 1
